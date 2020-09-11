c594e3bd5ed01c74d841c095fa717977924a4094
Coder-RKV <rk.aka.kishan@gmail.com>
2020-09-10 20:34:35 -0400

    Initial check-in of module active-directory-b2c-Xamarin-native

---
+++ UserDetailsClient/UserDetailsClient.iOS/ParentWindowLocatorService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Identity.Client;
+using UIKit;
+using UserDetailsClient.Core.Features.LogOn;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(UserDetailsClient.iOS.ParentWindowLocatorService))]
+namespace UserDetailsClient.iOS
+{
+    class ParentWindowLocatorService : IParentWindowLocatorService
+    {
+        public object GetCurrentParentWindow()
+        {
+            var window = UIApplication.SharedApplication.KeyWindow;
+            var vc = window.RootViewController;
+            
+            return vc;
+        }
+    }
+}
